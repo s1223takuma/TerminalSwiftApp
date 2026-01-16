2ecf5054ca0db9f2772f1564a2432efd94ce2873
soda <114852162+s1223takuma@users.noreply.github.com>
2026-01-16 23:26:21 +0900

    fix

--- ButtonApp/ContentView.swift
+++ ButtonApp/ContentView.swift
@@ -2,6 +2,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello from terminal")
+        Text("Hello world")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.white)
+            .ignoresSafeArea()
     }
+}
+
+#Preview{
+    ContentView()
 }
